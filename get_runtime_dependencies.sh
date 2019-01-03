d7fb3a221f36265804f09417826c77af12e3e4b1
Ankan Poddar <friendyankan@gmail.com>
2019-01-03 19:00:38 +0530

    Modify get_runtime_dependencies script to remove duplications

--- get_runtime_dependencies.sh
+++ get_runtime_dependencies.sh
@@ -7,10 +7,18 @@ base_image=$2
 input_file="/root/runtime_binaries.txt"
 output_file="/root/runtime_applications.yaml"
 
+elementIn () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
 if [[ $package != '' ]]; then
 
   ldd "$package" > "$input_file"
   echo "base_packages:" > "$output_file"
+  applications_array=$()
 
   while IFS= read -r line
   do
@@ -26,29 +34,35 @@ if [[ $package != '' ]]; then
 
       if [[ $base_image == "alpine" ]]; then
         application=$(apk info --who-owns "$var" | awk '{print $5}')
-        echo "$application"
-        echo "$application" | sed 's/\(.*\)-\([0-9].*\)/  - \1: \2/' >> "$output_file"
+        if (! elementIn "$application" "${applications_array[@]}"); then
+          echo "$application" | sed 's/\(.*\)-\([0-9].*\)/  - \1: \2/' >> "$output_file"
+          applications_array+=($application)
+        fi
       elif [[ $base_image == "centos" ]]; then
         application=$(rpm -qf "$var")
-        if [[ $application == *"fc"* ]]; then
-          echo "$application" | sed s'/\(.*\)-\([0-9]*\(\.[0-9]*\)*-[0-9].*\)\.fc.*/  - \1: \2/' >> "$output_file"
-        elif [[ $application == *"el"* ]]; then
-          echo "$application" | sed s'/\(.*\)-\([0-9]*\(\.[0-9]*\)*-[0-9].*\)\.el.*/  - \1: \2/' >> "$output_file"
+        if (! elementIn "$application" "${applications_array[@]}"); then
+          if [[ $application == *"fc"* ]]; then
+            echo "$application" | sed s'/\(.*\)-\([0-9]*\(\.[0-9]*\)*-[0-9].*\)\.fc.*/  - \1: \2/' >> "$output_file"
+          elif [[ $application == *"el"* ]]; then
+            echo "$application" | sed s'/\(.*\)-\([0-9]*\(\.[0-9]*\)*-[0-9].*\)\.el.*/  - \1: \2/' >> "$output_file"
+          fi
+          applications_array+=($application)
         fi
       elif [[ $base_image == "debian" ]]; then
         application=$(dpkg -S "$var" | awk '{print $1}' | sed s'/:$//')
         if [[ $application == '' ]]; then
           application=$(dpkg -S "/usr$var" | awk '{print $1}' | sed s'/:$//')
         fi
-        version=$(apt-cache policy "$application" | grep 'Installed' | awk '{print $2}')
-        if [[ $version == *"ubuntu"* ]]; then
-          version=$(echo "$version" | sed s'/ubuntu.*//')
+        if (! elementIn "$application" "${applications_array[@]}"); then
+          version=$(apt-cache policy "$application" | grep 'Installed' | awk '{print $2}')
+          if [[ $version == *"ubuntu"* ]]; then
+            version=$(echo "$version" | sed s'/ubuntu.*//')
+          fi
+          echo "  - $application: $version" >> "$output_file"
+          applications_array+=($application)
         fi
-        echo "  - $application: $version" >> "$output_file"
       fi
     fi
   done < "$input_file"
-
-  rm "$input_file"
 
 fi
